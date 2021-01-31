0cbbdfe800ca604a68a57bba6134ca1d112cd297
juanmartinez238 <juan.martinez238@yahoo.com>
2021-01-31 10:28:18 -0500

    created user movie components for saving movies

--- client/src/components/SearchResults.jsx
+++ client/src/components/SearchResults.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Movie from './Movie';
+import UserMovie from './UserMovie';
 
 const SearchResults = (props) => {
     const {search} = props;
     return (
         <div className='movie-container'>
             {search.length > 0 && search.map(movie => 
-            <Movie key={movie.id} {...movie} />
+            <UserMovie key={movie.id} {...movie} />
             )}
         </div>
     )

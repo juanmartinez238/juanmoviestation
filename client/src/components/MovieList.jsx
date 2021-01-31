c94aeae6e847baf4e29f105adf3e9aff6b454c07
juanmartinez238 <juan.martinez238@yahoo.com>
2021-01-31 09:52:22 -0500

    refactored movielist component to make server api call

--- client/src/components/MovieList.jsx
+++ client/src/components/MovieList.jsx
@@ -1,24 +1,38 @@
 import React, {useEffect, useState} from 'react';
 import Movie from './Movie';
-const {REACT_APP_MOVIE_API_KEY} = process.env; 
+import axios from 'axios';
+// const {REACT_APP_MOVIE_API_KEY} = process.env; 
 
 
-const API_KEY = process.env.REACT_APP_MOVIE_API_KEY;
-const FEATURED_API =`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}`;
+// const API_KEY = process.env.REACT_APP_MOVIE_API_KEY;
+// const FEATURED_API =`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}`;
 
 
 const MovieList = () => {
     const [movies, setMovies] = useState([]);
 
-    console.log(REACT_APP_MOVIE_API_KEY)
+    // console.log(REACT_APP_MOVIE_API_KEY)
 
-    useEffect( () => {
-    fetch(FEATURED_API).then(res => res.json())
-    .then(data => {
-        console.log(data)
-        setMovies(data.results);
-    });
-    },[])   
+    // useEffect( () => {
+    // fetch(FEATURED_API).then(res => res.json())
+    // .then(data => {
+    //     console.log(data)
+    //     setMovies(data.results);
+    // });
+    // },[])   
+
+    useEffect(async () => {
+        const fetchData = async () => {
+            const result = await axios.get(`/tmdb`);
+            console.log('this is result', result.data.results)
+            setMovies(result.data.results);
+        };
+        try{
+            fetchData();
+        }catch (err){
+            console.log(err);
+        }
+    },[setMovies]);
     
     return (
         
--- server.js
+++ server.js
@@ -5,12 +5,35 @@ const axios = require('axios');
 const app = require('./server/app'),
   port = process.env.PORT || 8080;
 
-  //CALL API FOR SEARCH PURPOSES
+const fetchTMDbAPI = async () => {
+  const API_KEY = process.env.REACT_APP_MOVIE_API_KEY;
+  const FEATURED_API =`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}`;
+  try{
+    const response = await axios.get(
+      FEATURED_API
+    )
+    return response;
+  }catch (err){
+    console.log(err);
+  }
+}
+
+  app.get('/tmdb', async (request, response) => {
+    try{
+      const data = await fetchTMDbAPI();
+      // console.log('this is data.data',data.data);
+      response.json(data.data);
+    }catch (err){
+      console.log(err);
+    }
+  })
+
+  //SEARCH- CALL API FOR SEARCH PURPOSES
   const searchTMDbAPI = async (searchTerm) => {
     const API_KEY = process.env.REACT_APP_MOVIE_API_KEY;
     const SEARCH_API = `https://api.themoviedb.org/3/search/movie?&api_key=${API_KEY}&query=${searchTerm}`;
-    console.log('this is API_KEY', API_KEY);
-    console.log('this is other searchTerm',searchTerm);
+    // console.log('this is API_KEY', API_KEY);
+    // console.log('this is other searchTerm',searchTerm);
     try{
       const resp = await axios.get(
         SEARCH_API
@@ -21,13 +44,13 @@ const app = require('./server/app'),
     }
   }
   
-  //gets data from the request sent in search component
+  //SEARCH- gets data from the request sent in search component
   app.get('/tmdb/search', async (request, response) => {
     const {searchTerm} = request.query;
-    console.log('this is searchTerm',searchTerm);
+    // console.log('this is searchTerm',searchTerm);
     try{
       const movieData = await searchTMDbAPI(searchTerm);
-      console.log('this is movieData.data',movieData.data)
+      // console.log('this is movieData.data',movieData.data)
       response.json(movieData.data)
     }catch (err) {
       console.log(err);

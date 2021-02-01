7e45ae5afbf8ccd93ed5d032352bf83893463365
juanmartinez238 <juan.martinez238@yahoo.com>
2021-02-01 11:14:02 -0500

    created forgot password page

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import PrivateRoute from './components/PrivateRoute';
 import Browse from './pages/Browse';
 import SearchPage from './pages/SearchPage';
 import ContactPage from './pages/ContactPage';
+import ForgotPassword from './pages/ForgotPassword';
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
             <Route exact path= '/' component= {Home} />
             <Route exact path='/login' component={Login} />
             <Route exact path='/createaccount' component={CreateAccount} />
+            <Route exact path= '/forgotpassword' component= {ForgotPassword} />
             <PrivateRoute exact path='/userhome' component={UserHome}/>
             <PrivateRoute exact path='/browse' component={Browse}/>
             <PrivateRoute exact path='/search' component={SearchPage}/>
